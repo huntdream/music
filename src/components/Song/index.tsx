fb1e024ce547c1352cfb847e8a45e4d88287a78c
Yu Mao <maoyulore@outlook.com>
2023-03-30 18:28:29 +0800

    tailwind css

--- src/components/Song/index.tsx
+++ src/components/Song/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
 import cls from 'classnames';
 import { ISong } from '../../types/song';
-import './style.scss';
 import usePlayer from '../../context/App/usePlayer';
 import Image from '../Image';
 
@@ -14,29 +13,40 @@ interface Props {
 const Song: React.FC<Props> = ({ song, className }) => {
   const { play } = usePlayer();
 
+  const handleNavigate = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className={cls('song', className)} onClick={() => play(song)}>
-      <div className='song-cover'>
+    <div
+      className={cls(
+        'flex p-1 border items-center cursor-pointer hover:bg-slate-50',
+        className
+      )}
+      onClick={() => play(song)}
+    >
+      <div className='border w-10 h-10 rounded overflow-hidden'>
         <Image
-          className='song-cover-img'
+          className='w-10 h-10'
           src={`${song.al?.picUrl}?param=50y50`}
           alt=''
         />
       </div>
-      <div className='song-info'>
-        <div className='song-name'>
+      <div className='ml-2'>
+        <div className=''>
           <span className='ellipsis' title={song.name}>
             {song.name}
           </span>
         </div>
-        <div className='song-aral'>
-          <div className='song-artists ellipsis'>
+        <div className='text-sm text-gray-500'>
+          <div className='ellipsis'>
             {song.ar?.map((ar) => (
               <Link
                 to={`/artist/${ar.id}`}
-                className='song-artist'
+                className='after:content-["/"] after:px-px last:after:content-[""] hover:underline'
                 key={ar.id}
                 title={ar.name}
+                onClick={handleNavigate}
               >
                 {ar.name}
               </Link>
