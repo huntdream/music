34bc6b5079f034f023f033c9f46686b5a61071c0
Yu Mao <maoyulore@outlook.com>
2023-03-30 19:02:31 +0800

    Add type

--- src/fetchers/usePlaylist.ts
+++ src/fetchers/usePlaylist.ts
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 import { IPlaylist } from '../types/playlist';
 
-const usePlaylist = (id?: number | string) => {
+const usePlaylist = (id?: number | string): { data: IPlaylist; error: any } => {
   const { data, error } = useSWR(id ? `/playlist/detail?id=${id}` : null);
 
   return { data: data?.playlist, error };
