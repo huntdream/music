0ea7408f93be008463281bbf3f97a5cd67190632
Yu Mao <maoyulore@outlook.com>
2024-04-17 21:35:10 +0800

    Replace http with https in Image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -3,7 +3,9 @@ import React, { ImgHTMLAttributes } from 'react';
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {}
 
 const Image: React.FC<Props> = ({ src, ...props }) => {
-  return <img src={src} {...props} loading='lazy' />;
+  return (
+    <img src={src?.replace(/http:/, 'https:')} {...props} loading='lazy' />
+  );
 };
 
 export default Image;
