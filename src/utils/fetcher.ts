bb41c8544c37fb21e5d84ff51db9019e8783c388
Yu Mao <maoyulore@outlook.com>
2024-08-27 21:43:59 +0800

    Handle error response

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -4,12 +4,17 @@ const fetcher = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
       ? 'https://netease.maoyu.dev'
-      : 'https://netease.maoyu.dev',
+      : 'http://localhost:3000',
   withCredentials: true,
 });
 
-fetcher.interceptors.response.use((response) => {
-  return response.data;
-});
+fetcher.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error?.response?.data);
+  }
+);
 
 export default fetcher;
