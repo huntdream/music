26a09f19bd831bd8bc8ba164b67a913df38eddcc
Yu Mao <maoyulore@outlook.com>
2024-08-27 21:44:20 +0800

    Support full width

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,23 +3,26 @@ import cls from 'classnames';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   pirmary?: boolean;
+  wide?: boolean;
 }
 
 const Button: React.FC<Props> = ({
   pirmary,
   className,
   children,
+  wide,
   ...props
 }) => {
   return (
     <button
       {...props}
       className={cls(
-        'h-9 px-4 font-medium rounded-md',
+        'h-8 px-4 font-medium rounded-md cursor-pointer disabled:opacity-75 disabled:cursor-not-allowed',
         className,
         pirmary
           ? 'bg-black text-white hover:bg-slate-700 '
-          : 'border border-slate-200 text-slate-900 hover:bg-slate-50'
+          : 'border border-slate-200 text-slate-900 hover:bg-slate-50',
+        wide && 'w-full'
       )}
     >
       {children}
