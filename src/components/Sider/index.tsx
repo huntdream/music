014cd2b7ff3666961984fe0320708a5aab01f432
Yu Mao <maoyulore@outlook.com>
2024-09-25 14:21:18 +0800

    Add key

--- src/components/Sider/index.tsx
+++ src/components/Sider/index.tsx
@@ -19,8 +19,6 @@ const Sider: React.FC<Props> = () => {
     navigate(path);
   };
 
-  console.log(id, pathname, 'id');
-
   const isActive = (path: string) => pathname === path;
 
   const menu = [
@@ -56,12 +54,13 @@ const Sider: React.FC<Props> = () => {
 
       <h2 className='my-2 ml-4 font-bold text-lg border-b'>我的歌单</h2>
       <div className='overflow-auto flex-1 pb-20 px-2'>
-        {mylist?.map((item) => (
+        {mylist?.map(({ id, name }) => (
           <Item
-            onClick={() => navigateTo(`/playlist/${item.id}`)}
-            active={item.id === playlistId}
+            onClick={() => navigateTo(`/playlist/${id}`)}
+            active={id === playlistId}
+            key={id}
           >
-            {item.name}
+            {name}
           </Item>
         ))}
       </div>
