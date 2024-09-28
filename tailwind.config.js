069bfcb11a758024aed8a94b895474d0736fa558
Yu Mao <maoyulore@outlook.com>
2024-09-28 21:50:53 +0800

    Add shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       animation: {
         'slide-in': 'slideIn 500ms ease-in-out',
       },
+      boxShadow: {
+        around: '0 10px 38px -10px #0e121659,0 10px 20px -15px #0e121633',
+      },
     },
   },
   plugins: [],
