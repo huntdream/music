e57841d8466947e7ff5f865bd4b5776ad32c49df
Yu Mao <maoyulore@outlook.com>
2024-11-25 19:21:36 +0800

    Add background

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -84,7 +84,7 @@ const SearchBar: React.FC<Props> = ({ initialKeyword = '' }) => {
           onPressEnter={handleKeyUp}
         />
         {open && result && (
-          <div className='absolute left-0 right-0 shadow-lg rounded-md p-2'>
+          <div className='absolute bg-white left-0 right-0 shadow-lg rounded-md p-2'>
             {result?.order ? (
               <div>
                 <h2 className='font-bold'>猜你想搜</h2>
