8ca5e4fd73b4dc9b51d3fc537c6033645c686203
Yu Mao <maoyulore@outlook.com>
2024-12-05 20:49:29 +0800

    Change outline size

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -18,7 +18,7 @@ const Input: React.FC<Props> = ({ label, className, ...props }) => {
       <div className='relative mt-2 rounded-md shadow-xs'>
         <input
           id={id}
-          className='block w-full rounded-md border-0 py-1.5  px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          className='block w-full rounded-md border-0 py-1.5  px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           {...props}
         />
       </div>
