6dbff727d276b304bd014264b59c59fff667d8e6
Yu Mao <maoyulore@outlook.com>
2024-12-17 18:55:25 +0800

    Providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
-import { BrowserRouter } from 'react-router-dom';
-import Config from './components/Config';
+import Providers from './components/Providers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
-    <Config>
-      <App />
-    </Config>
-  </BrowserRouter>
+  <Providers>
+    <App />
+  </Providers>
 );
